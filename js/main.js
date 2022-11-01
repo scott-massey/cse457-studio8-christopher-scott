9b220e1d4826a2493f470518e93b01a45c32e307
Scott Massey <scott.massey@wustl.edu>
2022-11-01 12:36:47 -0500

    Basics

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-d3.select("body").append("p").text("Hello World!")
+const readData = async () => {
+  let popularNamesData = await d3.csv("data/top_10_popular_names_borough.csv")
+  popularNamesData = popularNamesData.map((d) => {
+    return {
+      name: d.AnimalName,
+      boroughName: d.BoroughName,
+      count: +d.n,
+    }
+  })
+}
+
+readData()
