9ac17b0497b62fe2fb0f22728fc6181e0d49122c
Scott Massey <scott.massey@wustl.edu>
2022-11-01 12:46:10 -0500

    little more setup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let data
+
 const readData = async () => {
   let popularNamesData = await d3.csv("data/top_10_popular_names_borough.csv")
   popularNamesData = popularNamesData.map((d) => {
@@ -7,6 +9,15 @@ const readData = async () => {
       count: +d.n,
     }
   })
+
+  data.popularNames = popularNamesData
+}
+
+const createVis = () => {
+  let popularNamesVis = new PopularNamesVis(
+    "popular-names-vis",
+    data.popularNames
+  )
 }
 
 readData()
---
+++ js/popularNames.js
@@ -0,0 +1,64 @@
+/*
+ * PopularNamesVis - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data						-- the actual data: perDayData
+ */
+
+PopularNamesVis = function (_parentElement, _data) {
+  this.parentElement = _parentElement
+  this.data = _data
+
+  this.initVis()
+}
+
+/*
+ * Initialize visualization (static content, e.g. SVG area or axes)
+ */
+
+PopularNamesVis.prototype.initVis = function () {
+  var vis = this
+
+  vis.margin = { top: 40, right: 0, bottom: 60, left: 60 }
+  ;(vis.width =
+    $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right),
+    (vis.height = 300 - vis.margin.top - vis.margin.bottom)
+
+  // SVG drawing area
+  vis.svg = d3
+    .select("#" + vis.parentElement)
+    .append("svg")
+    .attr("width", vis.width + vis.margin.left + vis.margin.right)
+    .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+    .append("g")
+    .attr(
+      "transform",
+      "translate(" + vis.margin.left + "," + vis.margin.top + ")"
+    )
+
+  // Scales and axes
+
+  // (Filter, aggregate, modify data)
+  vis.wrangleData()
+}
+
+/*
+ * Data wrangling
+ */
+
+PopularNamesVis.prototype.wrangleData = function () {
+  var vis = this
+
+  this.displayData = this.data
+
+  // Update the visualization
+  vis.updateVis()
+}
+
+/*
+ * The drawing function - should use the D3 update sequence (enter, update, exit)
+ * Function parameters only needed if different kinds of updates are needed
+ */
+
+PopularNamesVis.prototype.updateVis = function () {
+  var vis = this
+}
