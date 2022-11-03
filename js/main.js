beace092574fc7ba2ac773c82662848dc35332c7
Scott Massey <scott.massey@wustl.edu>
2022-11-03 12:50:28 -0500

    Force directed graph

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let data
+let data = {}
 
 const readData = async () => {
   let popularNamesData = await d3.csv("data/top_10_popular_names_borough.csv")
@@ -9,15 +9,23 @@ const readData = async () => {
       count: +d.n,
     }
   })
-
   data.popularNames = popularNamesData
+
+  let breedsBoroughData = await d3.csv("data/breeds_borough_count.csv")
+  breedsBoroughData = breedsBoroughData.map((d) => {
+    return {
+      breedName: d.BreedName,
+      boroughName: d.BoroughName,
+      count: +d.Count,
+    }
+  })
+  data.breedsBorough = breedsBoroughData
+
+  createVis()
 }
 
 const createVis = () => {
-  let popularNamesVis = new PopularNamesVis(
-    "popular-names-vis",
-    data.popularNames
-  )
+  let breedsBorough = new BreedsBorough("breeds-borough", data.breedsBorough)
 }
 
 readData()
