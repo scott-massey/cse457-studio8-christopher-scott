f7a42c67df0d0aa0d03351a5f9febf8316960b2d
Christopher Patrick <cpatrick67135@gmail.com>
2022-11-04 14:58:10 -0500

    ages vis done

--- js/dogAgesVis.js
+++ js/dogAgesVis.js
@@ -24,7 +24,7 @@ DogAgesVis.prototype.initVis = function(){
 	vis.margin = { top: 40, right: 0, bottom: 60, left: 60 };
 
 	vis.width = 1000 - vis.margin.left - vis.margin.right,
-	vis.height = 600 - vis.margin.top - vis.margin.bottom;
+	vis.height = 1000 - vis.margin.top - vis.margin.bottom;
 
 	// SVG drawing area
 	vis.svg = d3.select("#" + vis.parentElement).append("svg")
@@ -34,9 +34,13 @@ DogAgesVis.prototype.initVis = function(){
 
 	console.log(vis.data)
 
+	var maxPop = Math.max(...vis.data.map(obj => obj.n))
 
+	var colors = d3.scaleLinear()
+		.domain([0, maxPop])
+		.range(["#fac9c5", "red"])
 
-	//queens, manhattan, bronx, staten island, brooklyn
+	//bronx
 	var boroughs = ["Queens", "Manhattan", "Bronx", "Staten Island", "Brooklyn"]
 	var bronxData = vis.data.slice(0, 18)
 
@@ -58,7 +62,7 @@ DogAgesVis.prototype.initVis = function(){
 		.append("rect")
 		.attr("class", "bar")
 		.attr("height", function(d, i){
-			return  ((d.n * .05) )
+			return  ((d.n * .03) )
 		})
 		.attr("width", 15)
 		.attr("x", function(d, i){
@@ -67,8 +71,20 @@ DogAgesVis.prototype.initVis = function(){
 			return xScaleBronx(i+ 1)
 		})
 		.attr("y", function(d, i){
-			return 200 - ((d.n * .05))
+			return 200 - ((d.n * .03))
 		})
+		.on("mouseover", function(d, i){
+			console.log(d)
+			console.log(i)
+			vis.svg.select("#hover-text").text("There are " + i.n + " " + (i.AgeAsOf2015) + "-year old dogs in " + i.Borough)
+		})
+		.on("mouseout", function(d, i){
+			vis.svg.select("#hover-text").text("Hover over a bar to see the population")
+		})
+		.attr("fill", function(d, i){
+			return colors(d.n)
+		})
+
 
 
 	//Brooklyn
@@ -93,7 +109,7 @@ DogAgesVis.prototype.initVis = function(){
 		.append("rect")
 		.attr("class", "bar")
 		.attr("height", function(d, i){
-			return  ((d.n * .05) )
+			return  ((d.n * .03) )
 		})
 		.attr("width", 15)
 		.attr("x", function(d, i){
@@ -102,26 +118,35 @@ DogAgesVis.prototype.initVis = function(){
 			return xScaleBrooklyn(i+ 1)
 		})
 		.attr("y", function(d, i){
-			return 200 - ((d.n * .05))
+			return 200 - ((d.n * .03))
 		})
-
-
+		.on("mouseover", function(d, i){
+			console.log(d)
+			console.log(i)
+			vis.svg.select("#hover-text").text("There are " + i.n + " " + (i.AgeAsOf2015) + "-year old dogs in " + i.Borough)
+		})
+		.on("mouseout", function(d, i){
+			vis.svg.select("#hover-text").text("Hover over a bar to see the population")
+		})
+		.attr("fill", function(d, i){
+			return colors(d.n)
+		})
 	//manhattan
 
 	var manhattanData = vis.data.slice(36, 54)
 
 	var manhattanGroup = vis.svg.append("g")
 		.attr("class", "manhattan")
-		.attr("x", 1000)
+		.attr("x", 500)
 		.attr("y", 500)
 
-	var xScaleManhattan = d3.scaleBand().range([0, 600])
+	var xScaleManhattan = d3.scaleBand().range([0, 300])
 		.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
 	var yScaleManhattan = d3.scaleLinear().range([vis.height, 0])
 		.domain([0, d3.max(manhattanData)])
 
 		manhattanGroup.append("g").call(d3.axisBottom(xScaleManhattan).ticks(10))
-		.attr("transform", "translate(0," + 200 + ")")
+		.attr("transform", "translate(0," + 400 + ")")
 
 
 		manhattanGroup.selectAll("rect")
@@ -130,7 +155,7 @@ DogAgesVis.prototype.initVis = function(){
 		.append("rect")
 		.attr("class", "bar")
 		.attr("height", function(d, i){
-			return  ((d.n * .05) )
+			return  ((d.n * .03) )
 		})
 		.attr("width", 15)
 		.attr("x", function(d, i){
@@ -139,82 +164,147 @@ DogAgesVis.prototype.initVis = function(){
 			return xScaleManhattan(i+ 1)
 		})
 		.attr("y", function(d, i){
-			return 200 - ((d.n * .05))
+			return 400 - ((d.n * .03))
 		})
-
-
+		.on("mouseover", function(d, i){
+			console.log(d)
+			console.log(i)
+			vis.svg.select("#hover-text").text("There are " + i.n + " " + (i.AgeAsOf2015) + "-year old dogs in " + i.Borough)
+		})
+		.on("mouseout", function(d, i){
+			vis.svg.select("#hover-text").text("Hover over a bar to see the population")
+		})
+		.attr("fill", function(d, i){
+			return colors(d.n)
+		})
 
 	//queens
 
-	// var bronxData = vis.data.slice(0, 18)
+	var queensData = vis.data.slice(54, 72)
 
-	// var bronxGroup = vis.svg.append("g")
-	// 	.attr("class", "bronx")
+	var queensGroup = vis.svg.append("g")
+		.attr("class", "manhattan")
+		.attr("x", 500)
+		.attr("y", 500)
 
-	// var xScale = d3.scaleBand().range([0, 300])
-	// 	.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
-	// var yScale = d3.scaleLinear().range([vis.height, 0])
-	// 	.domain([0, d3.max(bronxData)])
+	var xScaleQueens = d3.scaleBand().range([400, 700])
+		.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+	var yScaleQueens = d3.scaleLinear().range([vis.height, 0])
+		.domain([0, d3.max(queensData)])
 
-	// bronxGroup.append("g").call(d3.axisBottom(xScale).ticks(10))
-	// 	.attr("transform", "translate(0," + vis.height + ")")
-
-
-	// bronxGroup.selectAll("rect")
-	// 	.data(bronxData)
-	// 	.enter()
-	// 	.append("rect")
-	// 	.attr("class", "bar")
-	// 	.attr("height", function(d, i){
-	// 		return  ((d.n * .1) )
-	// 	})
-	// 	.attr("width", 15)
-	// 	.attr("x", function(d, i){
-	// 		console.log(i)
-	// 		console.log(d)
-	// 		return xScale(i+ 1)
-	// 	})
-	// 	.attr("y", function(d, i){
-	// 		return 200 - ((d.n * .1))
-	// 	})
+		queensGroup.append("g").call(d3.axisBottom(xScaleQueens).ticks(10))
+		.attr("transform", "translate(0," + 400 + ")")
 
 
+		queensGroup.selectAll("rect")
+		.data(queensData)
+		.enter()
+		.append("rect")
+		.attr("class", "bar")
+		.attr("height", function(d, i){
+			return  ((d.n * .03) )
+		})
+		.attr("width", 15)
+		.attr("x", function(d, i){
+			console.log(i)
+			console.log(d)
+			return xScaleQueens(i+ 1)
+		})
+		.attr("y", function(d, i){
+			return 400 - ((d.n * .03))
+		})
+		.on("mouseover", function(d, i){
+			console.log(d)
+			console.log(i)
+			vis.svg.select("#hover-text").text("There are " + i.n + " " + (i.AgeAsOf2015) + "-year old dogs in " + i.Borough)
+		})
+		.on("mouseout", function(d, i){
+			vis.svg.select("#hover-text").text("Hover over a bar to see the population")
+		})
+		.attr("fill", function(d, i){
+			return colors(d.n)
+		})
 	
 	//staten island
 
-	// var bronxData = vis.data.slice(0, 18)
+	var statenData = vis.data.slice(72, 90)
 
-	// var bronxGroup = vis.svg.append("g")
-	// 	.attr("class", "bronx")
+	var statenGroup = vis.svg.append("g")
+		.attr("class", "manhattan")
+		.attr("x", 500)
+		.attr("y", 500)
 
-	// var xScale = d3.scaleBand().range([0, 300])
-	// 	.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
-	// var yScale = d3.scaleLinear().range([vis.height, 0])
-	// 	.domain([0, d3.max(bronxData)])
+	var xScaleStaten = d3.scaleBand().range([200, 500])
+		.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+	var yScaleStaten = d3.scaleLinear().range([vis.height, 0])
+		.domain([0, d3.max(statenData)])
 
-	// bronxGroup.append("g").call(d3.axisBottom(xScale).ticks(10))
-	// 	.attr("transform", "translate(0," + vis.height + ")")
+		statenGroup.append("g").call(d3.axisBottom(xScaleStaten).ticks(10))
+		.attr("transform", "translate(0," + 600 + ")")
 
 
-	// bronxGroup.selectAll("rect")
-	// 	.data(bronxData)
-	// 	.enter()
-	// 	.append("rect")
-	// 	.attr("class", "bar")
-	// 	.attr("height", function(d, i){
-	// 		return  ((d.n * .1) )
-	// 	})
-	// 	.attr("width", 15)
-	// 	.attr("x", function(d, i){
-	// 		console.log(i)
-	// 		console.log(d)
-	// 		return xScale(i+ 1)
-	// 	})
-	// 	.attr("y", function(d, i){
-	// 		return 200 - ((d.n * .1))
-	// 	})
+		statenGroup.selectAll("rect")
+		.data(statenData)
+		.enter()
+		.append("rect")
+		.attr("class", "bar")
+		.attr("height", function(d, i){
+			return  ((d.n * .03) )
+		})
+		.attr("width", 15)
+		.attr("x", function(d, i){
+			console.log(i)
+			console.log(d)
+			return xScaleStaten(i+ 1)
+		})
+		.attr("y", function(d, i){
+			return 600 - ((d.n * .03))
+		})
+		.on("mouseover", function(d, i){
+			console.log(d)
+			console.log(i)
+			vis.svg.select("#hover-text").text("There are " + i.n + " " + (i.AgeAsOf2015) + "-year old dogs in " + i.Borough)
+		})
+		.on("mouseout", function(d, i){
+			vis.svg.select("#hover-text").text("Hover over a bar to see the population")
+		})
+		.attr("fill", function(d, i){
+			return colors(d.n)
+		})
 
-	
+	//add location name to each chart
+	vis.svg.append("text")
+		.text("The Bronx")
+		.attr("x", 100)
+		.attr("y", 250)
+
+	vis.svg.append("text")
+		.text("Brooklyn")
+		.attr("x", 500)
+		.attr("y", 250)
+
+	vis.svg.append("text")
+		.text("Manhattan")
+		.attr("x", 100)
+		.attr("y", 450)
+
+	vis.svg.append("text")
+		.text("Queens")
+		.attr("x", 500)
+		.attr("y", 450)
+
+	vis.svg.append("text")
+		.text("Staten Island")
+		.attr("x", 300)
+		.attr("y", 650)
+
+	//hover text
+	vis.svg.append("text")
+		.text("Hover over a bar to see the population")
+		.attr("x", 200)
+		.attr("y", 50)
+		.attr("font-size", 20)
+		.attr("id", "hover-text")
 
 	// (Filter, aggregate, modify data)
 	vis.wrangleData();
